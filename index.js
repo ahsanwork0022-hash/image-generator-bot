deda0c8e3c745dbb98202c1968182139b4f3700a
ahsanwork0022-hash <ahsanwork0022@gmail.com>
2025-11-14 15:37:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
-const token = "BOT_TOKEN";
-const admin = "USERID";
-const DATABASE_URL = "DATABASE_URL"; 
+const token = "8521928941:AAEMEI3ZIy21wAXHZ7DAIVftc_wlELDUXTk";
+const admin = "8521928941";
+const DATABASE_URL = "https://anime-d7adb-default-rtdb.firebaseio.com"; 
 const WEBHOOK_URL = "VERCEL_URL";
 
 const bot = new TelegramBot(token, { webHook: { port: false } });
@@ -118,11 +118,11 @@ app.post("/", async (req, res) => {
       const caption =
         "*👆 Here Is Your Generated Image\n\n💭 Your Prompt:*\n`" +
         msg.text +
-        "`\n\n*🧑‍💻 Created By:* [BOTNAME](https://telegram.dog/BOT_USERNAME)";
+        "`\n\n*🧑‍💻 Created By:* [BOTNAME](https://telegram.dog/Anime_image_generator_bot)";
       const keyboard = {
         reply_markup: {
           inline_keyboard: [
-            [{ text: "🧑‍💻 Developer", url: "https://telegram.dog/DEVELOPER" }]
+            [{ text: "🧑‍💻 Developer", url: "https://telegram.dog/Ahsan_bai" }]
           ]
         },
         parse_mode: "Markdown",
